bc2630a0535965862e5538ea329844b9a22b53c2
jao-victor1 <joaovictordemoura664@gmail.com>
2025-03-27 17:40:16 -0300

    commit

--- atividade1/imc.c
+++ atividade1/imc.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
+#include <math.h>
 
 
     int main(int argc, char* argv[]){
+
+    float altura;
+    float peso;
+   
+
+    printf ("Digite uma altura.\n");
+    scanf ("%f", &altura);
+
+    printf ("Digite um peso.\n");   
+    scanf ("%f", &peso);
+
+    float imc = peso / (pow (altura, 2));
+
+    printf ("O IMC eh: %.2f\n", imc);
+
+    system("pause");
 
    
     return 0;
