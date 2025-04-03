97a4f47979a266a5f5d92abd41767cd2b8199ee6
jao-victor1 <joaovictordemoura664@gmail.com>
2025-04-03 11:21:35 -0300

    ghvkm

--- tipos de dados/testeswitch.c
+++ tipos de dados/testeswitch.c
@@ -5,12 +5,18 @@
         float a, b;
         
 
-        printf ("selecione uma opcao\n1 = subrtracao.\n2 = adicao.\n");
+        printf ("selecione uma opcao\n1 = subrtracao.\n2 = adicao.\n3 = multiplicacao.\n4 = divisao.\n");
+    
         scanf ("%d", &i);
+        if (i < 4){
+            printf ("erro");
+        } else{
         printf("selecione numeros\n");
         scanf ("%f %f", &a, &b);
         int adicao = a + b;
         int subtracao = a - b;
+        int mult = a * b;
+        float div = a / b;
 
         
         
@@ -25,11 +31,19 @@
         printf ("a adicao eh: %d\n", adicao);
         break;
 
+        case 3:
+        printf ("a multiplicacao eh: %d\n", mult);
+        break;
+
+        case 4:
+        printf ("a divisao eh: %f\n", div);
+        break;
+
         
         default:
             printf ("erro.\n");
         break;
-
+        }
         }
         
 
