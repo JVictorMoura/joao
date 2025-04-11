d8e9a5074549e2001d0246135983e0035194ac23
jao-victor1 <joaovictordemoura664@gmail.com>
2025-04-11 11:16:00 -0300

    asd

--- vetores/main.c
+++ vetores/main.c
@@ -5,11 +5,11 @@
 int main(int argc, char *argv[])
 {
     int n;
-    for (int n = 0; n >= 0 && n <= 100; n++)
+    for (int n = 0; n >= 0 && n <= 100; n += 10) //"+="" para selecionar a quantidade a ser adicionada
     {
         system("cls");
         printf("\n%d", n);
-        Sleep(500);
+        Sleep(700);
     }
 
     return 0;
