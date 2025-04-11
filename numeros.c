98185dabd0674a22add90c90d462f03685dc8394
jao-victor1 <joaovictordemoura664@gmail.com>
2025-04-11 11:01:59 -0300

    asd

---
+++ numeros.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+    int main(int argc, char* argv[]){
+        int n;
+        for (int n = 0; n >= 0 && n <= 100; n++){
+        printf ("\n%d", n);
+        }
+   
+    return 0;
+}
