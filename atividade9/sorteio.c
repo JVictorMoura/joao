a619256f38d045453aab78acb9a7380c12897083
jao-victor1 <joaovictordemoura664@gmail.com>
2025-06-06 12:04:33 -0300

    asd

---
+++ atividade9/sorteio.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+    int main(int argc, char* argv[]){
+       int min = atoi(argv[1]);
+    int max = atoi(argv[2]);
+    int esperado = atoi(argv[3]);
+if(argc == 4) {
+  
+         printf ("");
+        return 0;
+}
+}
